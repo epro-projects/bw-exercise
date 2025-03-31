2611ed539826daa2e1f073c38bbc35e30d8534fe
epro-projects <erictimprovencio@gmail.com>
2025-03-31 13:50:42 -0500

    Create stg_source_3.sql

---
+++ models/staging/stg_source_3.sql
@@ -0,0 +1,35 @@
+WITH raw AS (
+  SELECT
+    TRIM(Operation) AS operation_id,
+    TRIM("Operation Name") AS operation_name,
+    TRIM(Address) AS address,
+    TRIM(City) AS city,
+    TRIM(State) AS state,
+    TRIM(Zip) AS postal_code,
+    TRIM(County) AS county,
+    TRIM(Phone) AS phone,
+    TRIM(Type) AS facility_type,
+    TRIM(Status) AS facility_status,
+    TRIM("Issue Date") AS issue_date,
+    CAST(Capacity AS INT) AS capacity,
+    TRIM("Email Address") AS email,
+    TRIM("Facility ID") AS facility_id
+  FROM {{ source('raw_files','source_3') }}
+)
+
+SELECT
+  operation_id,
+  operation_name,
+  address,
+  city,
+  state,
+  postal_code,
+  county,
+  phone,
+  facility_type,
+  facility_status,
+  issue_date,
+  capacity,
+  email,
+  facility_id
+FROM raw;
